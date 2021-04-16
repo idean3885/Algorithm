b48397b4875a0f9215eba53acb1871c427f1ae97
idean <idean@211.214.193.170>
2021-04-17 00:20:26 +0900

    fix: 백준 알고리즘 dp 1149번 Top-Down 방식(재귀함수)로 해결

--- AlgorithmPractice/src/com/dykim/baekjoon/dp/_1149/Main.java
+++ AlgorithmPractice/src/com/dykim/baekjoon/dp/_1149/Main.java
@@ -30,12 +30,16 @@ import java.util.Arrays;
  *     마지막까지 탐색한 경우 이미 최종 비용이 계산된 경우이기 때문에 해당 값을 저장 후 리턴하도록 수정 -> 실패
  * 
  * 2. 풀이 결과
- *  소요시간: 분 (15:15 ~ 16:22) 
+ *  소요시간: 1시간 7분 (15:15 ~ 16:22) 
  *  결과: 실패
  *  
  * 3. 문제 해결
+ *  f(n/rgb) = f(n+1/rgb) + cost 로 RGB 경우의 수에 따라 3가지 가중치를 따로 저장하는 문제였다.(Top-Down 방식)
+ *  n 번째 마다 R,G,B 인 경우가 각각 존재하며 중복되지 않아야하는 조건식도 있기 때문에 재귀함수 호출 시, 잘 확인해야 하는 문제. 
  *   
  * 4. 개선할 점
+ *  기존 dp Top-Down 방식에 함수 호출마다 조건에 맞게 경우의 수를 잘 따져야했는데, 그 과정에서 헷갈렸다.
+ *  Top -> Down 방향으로 풀이할 경우, Down 에서 리턴되는 값들을 저장해야 한다는 점을 잘 숙지해야겠다.
  *  
  * @since 2021.04.16
  */
@@ -68,45 +72,36 @@ public class Main {
         
         int min = 999999999;
         for (int i=0; i<3; i++) {
-            int cost = dfs(0, i, costMap[0][i]);
+            int cost = dfs(0, i);
             min = cost<min? cost:min;
         }
         
         System.out.println(min);
     }
     
-    private static int dfs(int p, int rgb, int cost) {
+    private static int dfs(int p, int rgb) {
         // 집 끝까지 다 온 경우
         if (p == memo.length-1) {
-            if (memo[p][rgb]==0 || memo[p][rgb]> cost) {
-                memo[p][rgb] = cost;
-            }
-            
+            memo[p][rgb] = costMap[p][rgb];
             return memo[p][rgb];
         }
         
         // 이미 계산된 경우
         if (memo[p][rgb]>0) {
-            // 이전에 계산된 값보다 큰 경우 갱신
-            if (memo[p][rgb] > cost) {
-                memo[p][rgb] = cost;
-            }
-            
             return memo[p][rgb];
         }
         
         int np = p +1;
         if (rgb == R) {
-            memo[p][rgb] =Math.min(dfs(np, G, cost + costMap[np][G]), dfs(np, B, cost + costMap[np][B]));
+            memo[p][rgb] = Math.min(dfs(np, G), dfs(np, B)) + costMap[p][rgb];
         }
         else if (rgb == G) {
-            memo[p][rgb] = Math.min(dfs(np, R, cost + costMap[np][R]), dfs(np, B, cost + costMap[np][B]));
+            memo[p][rgb] = Math.min(dfs(np, R), dfs(np, B)) + costMap[p][rgb];
         }
         else {
-            memo[p][rgb] = Math.min(dfs(np, R, cost + costMap[np][R]), dfs(np, G, cost + costMap[np][G]));
+            memo[p][rgb] = Math.min(dfs(np, R), dfs(np, G)) + costMap[p][rgb];
         }
         
-        // 마지막까지 다녀온 값은 가중치를 모두 들고 있기 때문에 그대로 리턴
         return memo[p][rgb];
     }
 }
